0a38098dc9dd79a36aa6c1700520653e9fb9ba80
Ahmet TORPİL <torpilahmet@gmail.com>
2025-07-11 12:13:33 +0300

    Sayfa yapısı Oluşturuldu

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from "./Pages/Home.jsx";
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <Home></Home>
   )
 }
 
